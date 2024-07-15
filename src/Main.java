1ca863108b2b65a63237f346f681293ad13d590f
ProkopenkoAV <arty1987@mail.ru>
2024-07-15 17:09:06 +0300

    complited

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        OnTaskDoneListener listener = System.out::println;
+
+        Worker worker = new Worker(listener);
+        worker.start();
     }
 }
---
+++ src/Worker.java
@@ -0,0 +1,13 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+}
